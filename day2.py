1f27defcb4029683a1be820a871c6549abfa5edd
ajiiprstyo <offcajistd88@gmail.com>
2024-06-06 12:30:59 +0700

    day2.py
    membuat inputan kartu tanda mahasiswa

---
+++ day2.py
@@ -0,0 +1,14 @@
+# Menerima input dari pengguna
+nama_depan = input("Masukkan nama depan: ")
+nama_belakang = input("Masukkan nama belakang: ")
+nim = input("Masukkan NIM: ")
+
+# Menggabungkan nama depan dan nama belakang menjadi nama lengkap
+nama_lengkap = nama_depan + " " + nama_belakang
+
+# Menampilkan output sesuai dengan format yang diinginkan
+print("\nPendidikan Teknik Informatika")
+print(f"Nama : {nama_lengkap}")
+print(f"Nim  : {nim}")
+print("Fakultas Keguruan dan Ilmu Pendidikan")
+print("\n" + "UNIVERSITAS MUHAMMADIYAH SURAKARTA".upper())
